b73d09e64c5c51ab0ab8d3f303905271d2a7e99c
Joao Guilherme <jgfalantearaujo@gmail.com>
2026-02-26 11:10:16 -0300

    feat: Lógica da query e o fetch pages

--- Laboratórios/Lab-01/fetch_repos.py
+++ Laboratórios/Lab-01/fetch_repos.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 GRAPHQL_URL = "https://api.github.com/graphql"
 TIMEOUT = 30
 
+TARGET_REPOS = 100
+PAGE_SIZE = 10
+
 load_dotenv()
 
 def get_token():
@@ -14,6 +17,71 @@ def get_token():
         print("Erro: defina GITHUB_TOKEN.")
         sys.exit(1)
     return token
+
+def build_query():
+    return """
+    query TopRepos($cursor: String, $first: Int!) {
+      search(
+        query: "stars:>10000 sort:stars-desc"
+        type: REPOSITORY
+        first: $first
+        after: $cursor
+      ) {
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        nodes {
+          ... on Repository {
+            name
+            nameWithOwner
+            url
+            stargazerCount
+            createdAt
+            pushedAt
+            primaryLanguage {
+              name
+            }
+            pullRequests(states: MERGED) {
+              totalCount
+            }
+            releases {
+              totalCount
+            }
+            issues {
+              totalCount
+            }
+            closedIssues: issues(states: CLOSED) {
+              totalCount
+            }
+          }
+        }
+      }
+    }
+    """
+def fetch_page(token, cursor=None):
+    headers = {"Authorization": f"Bearer {token}"}
+    payload = {
+        "query": build_query(),
+        "variables": {
+            "cursor": cursor,
+            "first": PAGE_SIZE
+        }
+    }
+
+    response = requests.post(GRAPHQL_URL, json=payload, headers=headers)
+
+    if response.status_code != 200:
+        print(f"Erro HTTP {response.status_code}: {response.text}")
+        sys.exit(1)
+
+    data = response.json()
+
+    if "errors" in data:
+        print("Erro GraphQL:", data["errors"])
+        sys.exit(1)
+
+    return data["data"]["search"]
 
 def main():
     token = get_token()

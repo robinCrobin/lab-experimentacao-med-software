f93c0eda40afeae7c5362255535e46df1a29fe74
robertaSophia <roberta.silva@brius.com.br>
2026-02-26 09:45:07 -0300

    feat: leitura do token do github vindo do .env

--- Laboratórios/Lab-01/fetch_repos.py
+++ Laboratórios/Lab-01/fetch_repos.py
@@ -1,8 +1,19 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_token():
+    token = os.environ.get("GITHUB_TOKEN")
+    if not token:
+        print("Erro: defina GITHUB_TOKEN.")
+        sys.exit(1)
+    return token
+
 def main():
-    # token = get_token()
-    print('main')
-
-
+    token = get_token()
+    print(token)
 
 if __name__ == "__main__":
     main()
